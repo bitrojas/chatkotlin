5f4e28d11ac6e13bebb9c85e2396f02991597e1c
bitrojas <bitbucket.org.01@gmail.com>
2024-06-30 12:51:05 -0400

    Firebase Integrated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -43,6 +47,10 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/orojas/chatkotlin/MainActivity.kt
+++ app/src/main/java/com/orojas/chatkotlin/MainActivity.kt
@@ -1,20 +1,84 @@
 package com.orojas.chatkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.orojas.chatkotlin.databinding.ActivityMainBinding
+import com.orojas.chatkotlin.fragments.FragmentChats
+import com.orojas.chatkotlin.fragments.FragmentProfile
+import com.orojas.chatkotlin.fragments.FragmentUsers
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        if(firebaseAuth.currentUser == null){
+            goLoginOptions()
         }
+
+        // Default ViewFragment
+        viewFragmentProfile()
+
+        binding.buttomNV.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.item_profile->{
+                    // Visualizar el Perfil
+                    viewFragmentProfile()
+                    true
+                }
+                R.id.item_users->{
+                    // Visualizar los Usuarios
+                    viewFragmentUsers()
+                    true
+                }
+                R.id.item_chats->{
+                    // Visualizar los Chats
+                    viewFragmentChats()
+                    true
+                }
+                else->{
+                    false
+                }
+            }
+        }
+    }
+
+    private fun goLoginOptions() {
+        startActivity(Intent(applicationContext, LoginActivityOption::class.java))
+    }
+
+    private fun viewFragmentProfile(){
+        binding.tvTitle.text = "Perfil"
+        val fragment = FragmentProfile()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Profile")
+        fragmentTransaction.commit()
+    }
+
+    private fun viewFragmentUsers(){
+        binding.tvTitle.text = "Usuarios"
+        val fragment = FragmentUsers()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Users")
+        fragmentTransaction.commit()
+    }
+
+    private fun viewFragmentChats(){
+        binding.tvTitle.text = "Chats"
+        val fragment = FragmentChats()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Chats")
+        fragmentTransaction.commit()
     }
 }
